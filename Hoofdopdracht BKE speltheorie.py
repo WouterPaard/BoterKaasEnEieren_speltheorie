2d00996e5f51cab6e2eea3c2aefad8a2f4ddceaa
WouterPaard <45482452+WouterPaard@users.noreply.github.com>
2020-12-15 19:50:58 +0100

    Add files via upload
    Er is een nieuwe speler aangemaakt. De speler speelt nog niet slim en geeft voor elke positie waarde 1.

--- Hoofdopdracht BKE speltheorie.py
+++ Hoofdopdracht BKE speltheorie.py
@@ -4,6 +4,13 @@ from bke import *
 class MyRandomAgent(EvaluationAgent):
     def evaluate(self, board, my_symbol, opponent_symbol):
         return random.randint(1, 500)
+
+class MySmartAgent(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        getal = 1
+        return getal
     
 my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent, player_x=my_random_agent)
+my_smart_agent = MySmartAgent()
+
+start(player_o=my_smart_agent, player_x=my_random_agent)
